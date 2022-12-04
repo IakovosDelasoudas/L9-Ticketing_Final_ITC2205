db8cb982986dcaef8363b9f56f63a883a487610a
Maximos <m.tsamplakos@acg.edu>
2022-12-04 17:45:10 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -42,11 +42,37 @@ public class Main {
                         System.out.println("Goodbye");
                         System.exit(0);
                     default:
+                        System.out.println("Please type 1 - 2");
+                }
+            } while (option < 1 || option > 2);
+        }
+    }
+    public static void showusermenu()throws IOException {
+        while (true) {
+        System.out.println("---- What do you want to so today? -----");
+        System.out.println("1. Submit ticket");
+        System.out.println("2. Search ticket");
+        System.out.println("3. Exit");
+        int option = 0;
+            do {
+            option = readInteger();
+                switch (option) {
+                    case 1:
+                        //Submitticket();
+                        break;
+                    case 2:
+                        //search();
+                        break;
+                    case 3:
+                        System.out.println("Goodbye");
+                        System.exit(0);
+                    default:
                         System.out.println("Please type 1 - 3");
                 }
             } while (option < 1 || option > 3);
         }
     }
+
     public static void main(String[] args) throws IOException {
             showMenu();
     }
